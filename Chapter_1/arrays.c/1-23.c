2ec0c76a4987604c2a804bce279d388758244d60
Nazareno-Benavidez <nazarenobenavidez2@gmail.com>
2025-05-24 15:58:28 -0300

    Day 9 - exercise 1-23 done

---
+++ Chapter_1/arrays.c/1-23.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+void my_get_code(char code[], char newcode[], int maxline);
+
+/* Exercise 1−23. Write a program to remove all comments from a C program. Don't forget to handle quoted
+strings and character constants properly. C comments don't nest. */
+int main()
+{
+    char code[MAXLINE]; // code with comments
+    char newcode[MAXLINE]; // code without comments
+
+    my_get_code(code, newcode, MAXLINE);
+
+    printf("\n%s\n", newcode);
+
+    return 0;
+}
+
+void my_get_code(char code[], char newcode[], int maxline)
+{
+    int c, i, k, j;
+    j = 0;
+
+    // Write code[]
+    for(i = 0; i < maxline-1 && (c = getchar()) != EOF; i++)
+        code[i] = c;
+    code[i] = '\0';
+
+    // Read code[] and write it in newcode[] removing comments
+    for(k = 0; k < i  && j < maxline-1; k++){// k = read variable (reads through code[]) j = write variable (writes code[k] value in newcode[])
+        // if double '/' comment is encountered, remove everything from the first '/' to '\n' (end of comment)
+        if(code[k] == '/' && code[k+1] == '/'){
+            // Find end of comment
+            while(code[k] != '\n') 
+                k++;
+            newcode[j] = code[k]; // Write '\n' in newcode[], omitting the whole comment
+        }
+        // Else if '/* */' comment is encountered, remove everything from "/*" to "*/" (end of comment)
+        else if(code[k] == '/' && code[k+1] == '*'){
+            k+=2; // get past "/*"
+            // Find end of comment
+            while((code[k] != '*' && code[k+1] != '/')) // while not at "*/"
+                k++;
+            k+=2; // get at code[*/ + 1]
+            newcode[j] = code[k]; // Write next code[]'s value after the end of the comment
+        }
+        // If not a comment
+        else    
+            newcode[j] = code[k]; // Write code[]'s value into newcode[]
+        j++;
+    }
+} 
