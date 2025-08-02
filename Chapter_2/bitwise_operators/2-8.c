884faab937cc9a0c5a31b13cb5319a1426184dc6
Nazareno-Benavidez <nazarenobenavidez2@gmail.com>
2025-08-01 22:31:48 -0300

    Day 3 - stream

---
+++ Chapter_2/bitwise_operators/2-8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+unsigned int right_rot(unsigned int x, int n);
+
+/* Exercise 2−8. Write a function rightrot(x,n) that returns the value of the integer x rotated to the right
+by n positions */
+int main()
+{
+    //unsigned int x = 
+
+    return 0;
+}
