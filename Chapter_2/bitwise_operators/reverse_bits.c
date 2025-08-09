fe2bf4a9297e90719eff7acb33878d50a613fc0b
Nazareno-Benavidez <nazarenobenavidez2@gmail.com>
2025-08-09 00:05:48 -0300

    Day 10 - stream

--- Chapter_2/bitwise_operators/reverse_bits.c
+++ Chapter_2/bitwise_operators/reverse_bits.c
@@ -6,7 +6,7 @@ int reverse_bits(int x);
 
 int main()
 {
-    int x = 1;
+    int x = 64;
 
     printf("%d", reverse_bits(x));
 
@@ -16,10 +16,17 @@ int main()
 int reverse_bits(int x)
 {
     int x_updated = 0;
-    unsigned int mask = 0;
+    unsigned int rightmost_bits = 0;
+    unsigned int leftmost_bits = 0;
 
-    for(int i = 0; i <= INT_BITS; i++){
-        mask = ((unsigned) x >> (INT_BITS-1-i))&1;
-        x_updated = x_updated | (mask << (INT_BITS-1-i));
+    for(int i = 0; i < INT_BITS; i++){
+        rightmost_bits = (((unsigned int) x >>i)&1) << (INT_BITS-1-i);
+        leftmost_bits = (((unsigned int) x >> (INT_BITS-1-i))&1) << i;
+
+        x_updated = rightmost_bits | leftmost_bits;
+       // printf("%d", x_updated);
+        //x_updated = x_updated | (bits << (INT_BITS-1-i));
     }
+
+    return x_updated;
 }
