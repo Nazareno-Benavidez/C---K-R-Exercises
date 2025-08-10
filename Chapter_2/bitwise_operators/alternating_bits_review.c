cad26bff11b258ab1438acc76723c7041bca751d
Nazareno-Benavidez <nazarenobenavidez2@gmail.com>
2025-08-10 11:14:26 -0300

    Day 11 - stream

--- Chapter_2/bitwise_operators/alternating_bits_review.c
+++ Chapter_2/bitwise_operators/alternating_bits_review.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
 #define INT_BITS (sizeof(int) * 8)
+
 unsigned int has_alternating_bits(int x);
 
 int main()
 {
-    int x = 1431655765; // -1431655766
+    int x = 1431655765; //-1431655766
 
     printf("%u", has_alternating_bits(x));
+
+    return 0;
 }
 
 unsigned int has_alternating_bits(int x)
 {
-    if((((unsigned)x >> (INT_BITS-1))==1) && ((unsigned)x + ((unsigned)x>>1)) == -1)
+    if((((unsigned int)x >> INT_BITS-1) == 1) && ((((unsigned int)x >> 1) ^ x) == -1))
         return 1;
-    
-    else if((((unsigned)x >> (INT_BITS-1))==0) && ((unsigned)x + ((unsigned)x<<1)) == -1)
+    else if ((((unsigned int) x  >> INT_BITS-1) == 0) && ((((unsigned int)x << 1) ^ x) == -1))
         return 1;
     else
         return 0;
